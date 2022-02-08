e48a0ca1a8799ba2aea163eecbad1314f982d293
Peter Rasmussen <peter@soundboks.com>
2022-02-08 20:36:34 +0100

    day3_1 working

--- day3_1/src/main.rs
+++ day3_1/src/main.rs
@@ -1,54 +1,36 @@
 use std::{fs};
 
-
 fn main() {
-    let contents = fs::read_to_string("test_input.txt")
+    let contents = fs::read_to_string("input.txt")
     .expect("Something went wrong reading the file");
     // Step 1: get iterator from splitting on character.
     let values: Vec<&str> = contents.split('\n').collect();
 
-    let mut gamma = 0;
-    let mut epsilon = 0;
+    const WIDTH: usize = 12;
 
-    let mut pos_0 = 0;
-    let mut pos_1 = 0;
-    let mut pos_2 = 0;
-    let mut pos_3 = 0;
-    let mut pos_4 = 0;
+    let mut gamma: isize = 0;
+    let mut epsilon: isize = 0;
+
+    let mut result: [isize; WIDTH] = [0; WIDTH];
 
     for v in values {
         if let Ok(val) =  usize::from_str_radix(v, 2) {
-            print!("{:05b} ", val);
-            if ((val & 1<<0)>>0) == 1 {
-                pos_0 += 1;
-            } else {
-                pos_0 -= 1;
+            for i in 0..WIDTH {
+                if ((val & 1<<i)>>i) == 1 {
+                    result[i] += 1;
+                } else {
+                    result[i] -= 1;
+                }
             }
-            if ((val & 1<<1)>>1) == 1 {
-                pos_1 += 1;
-            } else {
-                pos_1 -= 1;
-            }
-            if ((val & 1<<2)>>2) == 1 {
-                pos_2 += 1;
-            } else {
-                pos_2 -= 1;
-            }
-            if ((val & 1<<3)>>3) == 1 {
-                pos_3 += 1;
-            } else {
-                pos_3 -= 1;
-            }
-            if ((val & 1<<4)>>4) == 1 {
-                pos_4 += 1;
-            } else {
-                pos_4 -= 1;
-            }
-
         }
     }
 
-    println!("{} {} {} {} {}", pos_4>0, pos_3>0, pos_2>0, pos_1>0, pos_0>0);
-
-    println!("gamma: {} - epsilon: {} - Result: {}", gamma, epsilon, gamma * gamma);
+    for (i, e) in result.iter().enumerate() {
+        if e > &0 {
+            gamma += 1<<i;
+        } else {
+            epsilon += 1<<i;
+        }
+    }
+    println!("gamma: {} - epsilon: {} - Result: {}", gamma, epsilon, gamma * epsilon);
 }
